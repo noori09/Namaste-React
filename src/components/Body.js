e50704d7e802936c1e0afb9d89cd056e4d0dfc03
noori09 <noorimehta09@gmail.com>
2023-01-15 04:47:52 +0530

    initial commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,31 @@
-import { restaurantList } from "./constants"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import RestaurantCard from "./RestaurantCard"
+import Shimmer from "./Shimmer"
 
-function filterData(searchText,restaurants){
-const filteredData= restaurants.filter((restaurant)=> restaurant.data.name.includes(searchText))
-console.log(filteredData)
-return filteredData
+function filterData(searchText, restaurants) {
+    const filteredData = restaurants.filter((restaurant) => 
+    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()))
+    return filteredData
 }
 
 export const Body = () => {
     const [searchText, setSearchText] = useState("")
-    const [restaurants, setRestaurants] = useState(restaurantList)
-    return (
+    const [filteredRestaurants, setFilteredRestaurants] = useState([])
+    const [allRestaurants, setAllRestaurants] = useState([])
+
+    useEffect(() => {
+        getRestaurants()
+    }, [])
+
+    async function getRestaurants() {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&page_type=DESKTOP_WEB_LISTING')
+        const json = await data.json()
+        setAllRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+        setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
+    }
+    //early return
+    if(!allRestaurants) return null;
+    return (allRestaurants.length ===0)? <Shimmer/>:(
         <>
             <div className="search-container">
                 <input
@@ -25,14 +39,15 @@ export const Body = () => {
                     className="search-btn"
                     onClick={() => {
                         //need to filter data
-                       const data= filterData(searchText,restaurants)
-                       setRestaurants(data);
+                        const data = filterData(searchText, allRestaurants)
+                        setFilteredRestaurants(data);
                     }}
                 >
                     Search</button>
             </div>
             <div className='restaurant-list'>
-                {restaurants.map((restaurant) => {
+                { filteredRestaurants.length ===0 ? <h1>No Restaurant Matches Your Filter</h1> :
+                filteredRestaurants.map((restaurant) => {
                     return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
                 })}
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 const Title = () => (
     <a href='/'>
         <img src='https://b.zmtcdn.com/data/pictures/chains/9/18933609/9fc9eb3e9765cb039a9d13596290d400.jpeg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A'
@@ -7,6 +9,7 @@ const Title = () => (
 )
 
 const Header = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     return (
         <div className='header'>
             <Title />
@@ -18,6 +21,10 @@ const Header = () => {
                     <li>Cart</li>
                 </ul>
             </div>
+            {isLoggedIn? 
+            <button className='button' onClick={()=>setIsLoggedIn(false)}>Logout</button>
+            :
+             <button className='button' onClick={()=>setIsLoggedIn(true)}>Login</button>}   
         </div>
     )
 }
