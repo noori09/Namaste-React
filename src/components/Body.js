a6d837cd3d9380e413205ea16e98c5c1d522b3ab
noori09 <noorimehta09@gmail.com>
2023-01-24 01:01:59 +0530

    dynamic url added for menu

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
 import RestaurantCard from "./RestaurantCard"
 import Shimmer from "./Shimmer"
+import {Link} from 'react-router-dom'
 
 function filterData(searchText, restaurants) {
-    const filteredData = restaurants.filter((restaurant) => 
-    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()))
+    const filteredData = restaurants.filter((restaurant) =>
+        restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase()))
     return filteredData
 }
 
@@ -17,7 +18,7 @@ export const Body = () => {
         getRestaurants()
     }, [])
 
-    
+
     async function getRestaurants() {
         const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&page_type=DESKTOP_WEB_LISTING')
         const json = await data.json()
@@ -25,8 +26,8 @@ export const Body = () => {
         setFilteredRestaurants(json?.data?.cards[2]?.data?.data?.cards)
     }
     //early return
-    if(!allRestaurants) return null;
-    return (allRestaurants.length ===0)? <Shimmer/>:(
+    if (!allRestaurants) return null;
+    return (allRestaurants.length === 0) ? <Shimmer /> : (
         <>
             <div className="search-container">
                 <input
@@ -47,10 +48,14 @@ export const Body = () => {
                     Search</button>
             </div>
             <div className='restaurant-list'>
-                { filteredRestaurants.length ===0 ? <h1>No Restaurant Matches Your Filter</h1> :
-                filteredRestaurants.map((restaurant) => {
-                    return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-                })}
+                {filteredRestaurants.length === 0 ? <h1>No Restaurant Matches Your Filter</h1> :
+                    filteredRestaurants.map((restaurant) => {
+                        return (
+                            <Link to={"/restaurant/" + restaurant.data.id} key={restaurant.data.id}>
+                                <RestaurantCard {...restaurant.data} />
+                            </Link>
+                        )
+                    })}
             </div>
         </>
     )
